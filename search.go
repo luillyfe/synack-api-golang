e370638e859ea700494de4a1b5ded154897ede4b
Fermin Blanco <luillyfe89@gmail.com>
2020-11-14 01:55:02 -0500

    feature: handle Http request and response.

--- search.go
+++ search.go
@@ -2,41 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
+	"net/http"
 )
 
-type Article struct {
-	Title string
-	Description []byte
-	Image []byte
-}
-
-func (a *Article) save() error {
-	filename := a.Title + ".txt"
-	return ioutil.WriteFile(filename, a.Description, 0600)
-}
-
-func readArticle(title string) (*Article, error) {
-	filename := title + ".txt"
-	description, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Article{Title: title, Description: description}, nil
-}
-
 func main() {
-	article := &Article{
-		Title: "conceived",
-		Description: []byte("It's about something you may be familiar with")}
-	article.save()
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
 
-	justRead, err := readArticle("conceived")
-	if err != nil {
-		fmt.Println("Something was wrong")
-	}
-
-	fmt.Println(justRead.Title)
-	fmt.Println(string(justRead.Description))
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
