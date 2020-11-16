b767460d6989255cdf27d4d0b77371dd59359477
Fermin Blanco <luillyfe89@gmail.com>
2020-11-16 17:56:03 -0500

    chore: env vars configuration chore: marshalling json objects feature: serving query results from google api

--- search.go
+++ search.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handleSearch)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func handleSearch(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Calling %s with query parameters, %v", r.URL.Path[1:], r.URL.Query())
+	query := r.URL.Query()
+	response := Response{}
+
+	resp, err := http.Get(viperEnvVariable("URI_GOOGLE") + query.Get("query"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	responseJson, _ := json.Marshal(response)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseJson)
+}
+
+type Response struct {
+	Items []Items `json:"items"`
+}
+
+type Items struct {
+	Title string `json:"title"`
+	Link string `json:"link"`
+	Snippet string `json:"snippet"`
 }
